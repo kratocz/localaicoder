32cb0cc0b56619a6c0ce01b1ceefe54d401775f8
Petr Kratochvíl <krato@krato.cz>
2025-07-08 12:55:20 +0200

    test

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 def main():
-    print("Hello from localaicoder!")
+    a = input()
+    watches = 123
+    if a == 1:
+        bitches = 1
+    print(watches)
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
